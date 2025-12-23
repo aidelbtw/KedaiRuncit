0158890f3a4f03cee5db6b29143777b6e36eb658
Aidel <aidelzaref@gmail.com>
2025-12-24 00:23:11 +0800

    add clock in/out, works for many employee (i think)

--- src/Attendance.java
+++ src/Attendance.java
@@ -1,14 +1,79 @@
+import java.time.*;
+import java.time.format.DateTimeFormatter;
 public class Attendance {
-    public static void record(Employee staff, DataManager dm){
-        System.out.println("===== Attendance Clock In =====");
-        System.out.println("Name: " + staff.getEmployeeName());
 
-        // Get outlet code from employee
+    private boolean isClockedIn = false;
+    private LocalDateTime clockInTime;
+    private LocalDateTime clockOutTime;
+
+    private String timeFormat(LocalDateTime time){
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("hh:mm a");
+        String formatted = time.format(fmt);
+        return formatted;
+    }
+
+    //This one is to connect with the main to see whether clocked in or out
+    public  boolean hasClockedIn(){
+        return clockInTime != null;
+    }
+
+    public void clockIn(Employee staff, DataManager dm){
+        if(isClockedIn){
+            System.out.println("You are already clocked in!");
+            return;
+        }
+        
+        // to get. the outlet code from Employee.java and name from DataManager.java using code
         String outletCode = staff.getOutlet();
-
-        // Get outlet name from DataManager
         String outletName = dm.getOutletName(outletCode);
 
-        System.out.println("Outlet: " + outletName);
+        clockInTime = LocalDateTime.now();
+        LocalDate today = LocalDate.now();
+        DateTimeFormatter dateFmt = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        isClockedIn = true;
+
+        System.out.println("===== Attendance Clock In =====");
+        System.out.println("Employee ID: " + staff.getEmployeeID());
+        System.out.println("Name: " + staff.getEmployeeName());
+        System.out.println("Outlet: (" + outletName + ")");
+        System.out.println("\nClock in Succeful!");
+        System.out.println("Date: " + today.format(dateFmt));
+        System.out.println("Time: " + timeFormat(clockInTime));
+    }
+
+    public void clockOut(Employee staff, DataManager dm){
+        if(!isClockedIn){
+            System.out.println("You have not clocked in!");
+            return;
+        }
+
+        String outletCode = staff.getOutlet();
+        String outletName = dm.getOutletName(outletCode);
+        isClockedIn = false;
+
+        //dis is 2 find the duration between clock in and out
+        clockOutTime = LocalDateTime.now();
+        double hoursWorked = Duration.between(clockInTime, clockOutTime).toMinutes()/60.0;
+        isClockedIn = false;
+        clockInTime = null;
+
+        LocalDate today = LocalDate.now();
+        DateTimeFormatter dateFmt = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        System.out.println("==== Attendance Clock Out =====");
+        System.out.println("Employee ID: " + staff.getEmployeeID());
+        System.out.println("Name: " + staff.getEmployeeName());
+        System.out.println("Outlet : " + outletName);
+        System.out.println("\nClock out succeful!");
+        System.out.println("Date: " + today.format(dateFmt));
+        System.out.println("Time: " + timeFormat(clockOutTime));
+        System.out.printf("Total Hours Worked: %.1f hours\n ", hoursWorked);
+    }
+
+    // This is for testing, to simulate time passed
+    public void simulateClockInHoursAgo(double hoursAgo) {
+    this.clockInTime = LocalDateTime.now().minusMinutes((long)(hoursAgo * 60));
+    this.isClockedIn = true;
+    System.out.println("Simulated clock-in " + hoursAgo + " hours ago.");
     }
 }
---
+++ src/AttendanceMenu.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+public class AttendanceMenu {
+ public static void menu(Employee staff, DataManager dm, Attendance attendance){        
+    Scanner input = new Scanner(System.in);
+        boolean goBack = false;
+        
+        while (!goBack){
+            System.out.println("\n===== Attendance Menu =====");
+
+            if(!attendance.hasClockedIn()){
+                System.out.println("1. Clock In");
+                System.out.println("2. Back");
+            } else {
+                System.out.println("1. Clock Out");
+                System.out.println("2. Back");
+            }
+
+            System.out.println("Choose: ");
+            int choice = input.nextInt();
+            input.nextLine();
+            if(!attendance.hasClockedIn()){
+                switch (choice) {
+                    case 1:
+                        attendance.clockIn(staff, dm);
+                        goBack = true;
+                        break;
+                    case 2:
+                        goBack = true;
+                        break;
+                    default:
+                        System.out.println("Invalid option");
+                        break;
+                }
+            } else {
+                switch (choice) {
+                    case 1:
+                        // This is if u want to test whether the hour counter is working
+                        // attendance.simulateClockInHoursAgo(5.5);
+                        attendance.clockOut(staff, dm);
+                        goBack = true;
+                        break;
+                    case 2:
+                        goBack = true;
+                    default:
+                        System.out.println("Invalid Option");
+                        break;
+                }
+            }
+        }
+    }
+}
