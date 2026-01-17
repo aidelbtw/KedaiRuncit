09255c915c1a65b5333dd7351118e49e73e2f889
Isaac Wong Zi Yang <24050251@siswa.um.edu.my>
2026-01-18 02:38:04 +0800

    Change menu option from 'Edit Profile/Product' to 'Edit System'

--- src/Main_ChangeOnlyYourPart.java
+++ src/Main_ChangeOnlyYourPart.java
@@ -56,7 +56,7 @@ public class Main_ChangeOnlyYourPart {
                         System.out.println("2. Stock Management");
                         System.out.println("3. Sales System");
                         System.out.println("4. Search Products");
-                        System.out.println("5. Edit Profile/Product");
+                        System.out.println("5. Edit System");
                         System.out.println("6. Analytics & Performance");
                         System.out.println("7. Register New Employee");
                         System.out.println("8. Logout");
